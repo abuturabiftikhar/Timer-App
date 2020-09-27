eafdf012fc808b83d28e9cf6855af9644e5829e3
Abuturab Iftikhar <63926829+abuturabiftikhar@users.noreply.github.com>
2020-09-27 22:05:07 +0500

    essd
    tdsw

--- src/components/Timer/Timer.spec.tsx
+++ src/components/Timer/Timer.spec.tsx
@@ -1,25 +1,25 @@
 import React from "react"
-import { shallow } from "enzyme"
+import { shallow, mount } from "enzyme"
 import Timer from "./Timer";
 
 describe("Timer", () => {
 
-    let container;
+    let container:any;
 
     beforeEach(() => (container = shallow(<Timer />)))
 
     it("should render a <div />", () => {
-        expect(container.find("div").length).toEqual(3)
+        expect(container.find("div").length).toBeGreaterThanOrEqual(1)
     });
 
     it("should render instances of the TimerButton component", () => {
-        expect(container.find("TimerButton").length).toBeGreaterThanOrEqual(1)
+        expect(container.find("TimerButton").length).toEqual(3)
       });
 
 });
 
 describe('mounted Timer', () => {
-    let container;
+    let container:any;
   
     beforeEach(() => (container = mount(<Timer />)));
   
